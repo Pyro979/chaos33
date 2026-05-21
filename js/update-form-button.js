3a966174b161d96f025af0691ad635cfe1865de1
Yuriy Shikhanovich <yuriy@viapeople.com>
2026-05-20 20:23:17 -0400

    form fix

--- js/update-form-button.js
+++ js/update-form-button.js
@@ -7,7 +7,7 @@
 (function() {
     'use strict';
 
-    var CONVENTION_DEFAULT = 'level_up_2026';
+    var CONVENTION_DEFAULT = 'dexlite_2026';
 
     function getPath() {
         return typeof window !== 'undefined' ? (window.location.pathname || '') : '';
